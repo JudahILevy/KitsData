54ab75c06a84c5783116dad648b8e5f72ca88a55
EphraimMeiri <75761279+EphraimMeiri@users.noreply.github.com>
2024-02-06 15:01:30 -0500

    Update Reformat.py
    Fixed to be a function (again) instead of hardcoding paths

--- Reformat.py
+++ Reformat.py
@@ -1,50 +1,50 @@
 import os
 import shutil
 
-
-# dataset_folder = input("Enter the absolute path to the dataset folder: ")
-
-# Replace 'path_to_your_dataset_folder' with the actual path to your dataset folder
-dataset_folder = '/Users/judahlevy/Downloads/kits23/dataset'
-destination_directory = '/Users/judahlevy/data/X/nnUnet_Data/nnUNet_raw/Task07_Kidney/'
-
-# Create destination_directory if it does not exist
-os.makedirs(destination_directory, exist_ok=True)
-
-# Create new folders if they don't exist
-images_tr_folder = os.path.join(destination_directory, 'imagesTr')
-labels_tr_folder = os.path.join(destination_directory, 'labelsTr')
-
-os.makedirs(images_tr_folder, exist_ok=True)
-os.makedirs(labels_tr_folder, exist_ok=True)
-
-# Iterate through each case folder in the dataset
-for case_folder in os.listdir(dataset_folder):
-    if not case_folder.startswith('case_'):
-        continue
-    print(case_folder)
-    case_folder_path = os.path.join(dataset_folder, case_folder)
-
-    # Check if it is a directory
-    if os.path.isdir(case_folder_path):
-        # Extract the numeric part from the case folder name
-        # numeric_part = ''.join(filter(str.isdigit, case_folder))
-        numeric_part = case_folder[6:]
-
-        # Construct the new filenames
-        imaging_filename = f'kidney_{numeric_part}.nii.gz'
-        segmentation_filename = f'kidney_{numeric_part}.nii.gz'
-
-        # Source paths
-        imaging_source_path = os.path.join(case_folder_path, 'imaging.nii.gz')
-        segmentation_source_path = os.path.join(case_folder_path, 'segmentation.nii.gz')
-
-        # Destination paths
-        imaging_dest_path = os.path.join(images_tr_folder, imaging_filename)
-        segmentation_dest_path = os.path.join(labels_tr_folder, segmentation_filename)
-
-        # Rename and move the files
-        shutil.copy(imaging_source_path, imaging_dest_path)
-        shutil.copy(segmentation_source_path, segmentation_dest_path)
-
-print("Conversion completed successfully.")
+def main(dataset_folder,nnRaw_folder):
+    # dataset_folder = input("Enter the absolute path to the dataset folder: ")
+    
+    # Replace 'path_to_your_dataset_folder' with the actual path to your dataset folder
+    
+    destination_directory = nnRaw_folder+'/Task07_Kidney/'
+    
+    # Create destination_directory if it does not exist
+    os.makedirs(destination_directory, exist_ok=True)
+    
+    # Create new folders if they don't exist
+    images_tr_folder = os.path.join(destination_directory, 'imagesTr')
+    labels_tr_folder = os.path.join(destination_directory, 'labelsTr')
+    
+    os.makedirs(images_tr_folder, exist_ok=True)
+    os.makedirs(labels_tr_folder, exist_ok=True)
+    
+    # Iterate through each case folder in the dataset
+    for case_folder in os.listdir(dataset_folder):
+        if not case_folder.startswith('case_'):
+            continue
+        print(case_folder)
+        case_folder_path = os.path.join(dataset_folder, case_folder)
+    
+        # Check if it is a directory
+        if os.path.isdir(case_folder_path):
+            # Extract the numeric part from the case folder name
+            # numeric_part = ''.join(filter(str.isdigit, case_folder))
+            numeric_part = case_folder[6:]
+    
+            # Construct the new filenames
+            imaging_filename = f'kidney_{numeric_part}.nii.gz'
+            segmentation_filename = f'kidney_{numeric_part}.nii.gz'
+    
+            # Source paths
+            imaging_source_path = os.path.join(case_folder_path, 'imaging.nii.gz')
+            segmentation_source_path = os.path.join(case_folder_path, 'segmentation.nii.gz')
+    
+            # Destination paths
+            imaging_dest_path = os.path.join(images_tr_folder, imaging_filename)
+            segmentation_dest_path = os.path.join(labels_tr_folder, segmentation_filename)
+    
+            # Rename and move the files
+            shutil.copy(imaging_source_path, imaging_dest_path)
+            shutil.copy(segmentation_source_path, segmentation_dest_path)
+    
+    print("Conversion completed successfully.")
